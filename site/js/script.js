75de83b6dd9c1c482ea3d75557b2cc36a7590f6d
Gabriella Xavier <51036491+gabriellaxavier05@users.noreply.github.com>
2023-04-06 17:29:58 -0300

    Atualizações arquivos

--- site/js/script.js
+++ site/js/script.js
@@ -1,3 +1,4 @@
+//menu
 //chamando const
 const h = document.querySelector(".h");
 const navMenu = document.querySelector(".nav-menu");
@@ -7,19 +8,3 @@ h.addEventListener("click", () => {
     h.classList.toggle('active');
     navMenu.classList.toggle('active');
 })
-
-
-//configurando o modal da tela inicial
-const openModalButton = document.querySelector("#open-modal");
-const closeModalButton = document.querySelector("#close-modal");
-const modal = document.querySelector("#modal");
-const fade = document.querySelector("#fade");
-
-//pra ocultar/desocultar o modal
-const toggleModal= () => {
-    [modal, fade].forEach((el) => el.classList.toggle("hide"));
-};
-
-[openModalButton, closeModalButton, fade].forEach((el) => {
-    el.addEventListener("click", () => toggleModal());
-});
